8edca3e66a235bc2337aea7fe7aca28112358b53
Lucas <lucasesteche13@gmail.com>
2024-05-03 10:41:09 -0300

    Se completo el archivo diaSemana

---
+++ assets/js/diaSemana.js
@@ -0,0 +1,25 @@
+let diaSemana = prompt("Ingrese un número del 1 al 7")
+if (diaSemana == 1){
+    console.log("Lunes")
+}
+if(diaSemana == 2){
+    console.log("Martes")
+}
+if(diaSemana == 3){
+    console.log("Miércoles")
+}
+if(diaSemana == 4){
+    console.log("Jueves")
+}
+if(diaSemana == 5){
+    console.log("Viernes")
+}
+if(diaSemana == 6){
+    console.log("Sábado")
+}
+if(diaSemana == 7){
+    console.log("Domingo")
+}
+if(diaSemana  >7){
+    console.log("El número ingresado es incorrecto")
+}
