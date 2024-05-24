343a52608e33fe7347df0253eb10cb623be5804d
Lucas <lucasesteche13@gmail.com>
2024-05-24 12:41:01 -0300

    Se finalizo el trabajo

--- Trabajo_Practico_3/assets/js/app.js
+++ Trabajo_Practico_3/assets/js/app.js
@@ -7,7 +7,18 @@ function agregarTar(tarNueva) {
     alert("Se añadió " + tarNueva + " a tu lista de tareas con éxito.");
     
 }
-function  lisTarea(params) {
+function  ediTarea(iTareas, modTarea, newTarea){
+    modTarea= prompt("¿Qué tarea desea modificar? \n" + Tareas)
+    iTareas= Tareas.indexOf(modTarea)
+    newTarea=prompt("¿Cuál será el nuevo nombre?")
+    Tareas.splice(iTareas, 1, newTarea)
+} 
+function elimTarea(tareas) {
+    modTarea= prompt("¿Qué tarea desea eliminar? \n" + Tareas)
+    iTareas= Tareas.indexOf(modTarea)
+    Tareas.splice(iTareas, 1)
+
+
     
 }
 
@@ -21,6 +32,17 @@ switch(accion){
     case 2:
         alert("Sus tareas guardadas: \n" + Tareas);
         break;
+    case 3:
+        ediTarea();
+        break;
+    case 4:
+        elimTarea();
+        break;
+    case 0:
+        alert("Usted salió del programa")
+        break;
+    default:
+        alert("ERROR: Valor no valido")
     
 }
 }while(accion !==0);
